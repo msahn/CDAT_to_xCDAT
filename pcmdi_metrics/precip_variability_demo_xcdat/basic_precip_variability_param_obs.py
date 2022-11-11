da593145acabe8a2fd4a06cefa13f99317573792
Min-Seop Ahn <ahn6@llnl.gov>
2022-11-10 16:50:41 -0800

    add tests for obs

--- pcmdi_metrics/precip_variability_demo_xcdat/basic_precip_variability_param_obs.py
+++ pcmdi_metrics/precip_variability_demo_xcdat/basic_precip_variability_param_obs.py
@@ -1,10 +1,9 @@
-mip = "cmip5"
-exp = "historical"
-mod = "pr.day.GISS-E2-H.historical.r6i1p1.20000101-20051231.nc"
+mip = "obs"
+mod = "pr.day.GPCP-1-3.PCMDI.gn.19961002-20170101.nc"
 var = "pr"
 frq = "day"
-modpath = 'demo_data/CMIP5_demo_timeseries/historical/atmos/day/pr/'
-results_dir = 'demo_output/precip_variability/GISS-E2-H/'
+modpath = 'demo_data/obs4MIPs_PCMDI_daily/NASA-JPL/GPCP-1-3/day/pr/gn/latest'
+results_dir = 'demo_output/precip_variability/GPCP-1-3/'
 prd = [2000,2005]  # analysis period
 fac = 86400  # factor to make unit of [mm/day]
 
